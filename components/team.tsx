035ede50769d3de1ee4ea128dfbcc3bc1e746c94
Frank <andreass.frankk@gmail.com>
2023-07-17 15:37:43 +0300

    fixed team

--- components/team.tsx
+++ components/team.tsx
@@ -17,8 +17,8 @@ export default function Team() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Our passionate team</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consequat.</p>
+            <h2 className="h2 mb-4">Echipa</h2>
+            {/* <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consequat.</p> */}
           </div>
 
           {/* Team members */}
@@ -27,80 +27,80 @@ export default function Team() {
             {/* 1st member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage01} width={120} height={120} alt="Team member 01" />
-                <h4 className="text-xl font-medium mb-1">Lara Lamprecht</h4>
-                <div className="text-gray-500 mb-1">CEO & Co-founder</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@laraamprecht</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage01} width={120} height={120} alt="Team member 01" /> */}
+                <h4 className="text-xl font-medium mb-1">Tut Alexandru</h4>
+                <div className="text-gray-500 mb-1">Backend Developer</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@laraamprecht</a> */}
               </div>
             </div>
 
             {/* 2nd member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage02} width={120} height={120} alt="Team member 02" />
-                <h4 className="text-xl font-medium mb-1">Marie Koniuszek</h4>
-                <div className="text-gray-500 mb-1">CEO & Co-founder</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@marie_moon</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage02} width={120} height={120} alt="Team member 02" /> */}
+                <h4 className="text-xl font-medium mb-1">Frank Andreas</h4>
+                <div className="text-gray-500 mb-1">CEO & Founder</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@marie_moon</a> */}
               </div>
             </div>
 
             {/* 3rd member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage03} width={120} height={120} alt="Team member 03" />
-                <h4 className="text-xl font-medium mb-1">Greg Sebastian</h4>
-                <div className="text-gray-500 mb-1">Head of Design</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@mr_sebastian</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage03} width={120} height={120} alt="Team member 03" /> */}
+                <h4 className="text-xl font-medium mb-1">Gabor Sebastian</h4>
+                <div className="text-gray-500 mb-1">Project Manager</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@mr_sebastian</a> */}
               </div>
             </div>
 
             {/* 4th member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage04} width={120} height={120} alt="Team member 04" />
-                <h4 className="text-xl font-medium mb-1">Timothy Chandran</h4>
+                {/* <Image className="rounded-full mb-4" src={TeamImage04} width={120} height={120} alt="Team member 04" /> */}
+                <h4 className="text-xl font-medium mb-1">Diana Deget</h4>
                 <div className="text-gray-500 mb-1">Product Executive</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@timothychandran</a>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@timothychandran</a> */}
               </div>
             </div>
 
             {/* 5th member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage05} width={120} height={120} alt="Team member 05" />
-                <h4 className="text-xl font-medium mb-1">Dominik Prasad</h4>
-                <div className="text-gray-500 mb-1">Backend Lead</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@dominikprasad</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage05} width={120} height={120} alt="Team member 05" /> */}
+                <h4 className="text-xl font-medium mb-1">David Krisztian</h4>
+                <div className="text-gray-500 mb-1">Frontend Developer</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@dominikprasad</a> */}
               </div>
             </div>
 
             {/* 6th member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage06} width={120} height={120} alt="Team member 06" />
-                <h4 className="text-xl font-medium mb-1">Darya Semenova</h4>
-                <div className="text-gray-500 mb-1">Backend Developer</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@daryamagic</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage06} width={120} height={120} alt="Team member 06" /> */}
+                <h4 className="text-xl font-medium mb-1">Alex Moholea</h4>
+                <div className="text-gray-500 mb-1">Hardware Technician</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@daryamagic</a> */}
               </div>
             </div>
 
             {/* 7th member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="600" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage07} width={120} height={120} alt="Team member 07" />
-                <h4 className="text-xl font-medium mb-1">Quentin Renvoye</h4>
-                <div className="text-gray-500 mb-1">Product Designer</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@quentinrenvoye</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage07} width={120} height={120} alt="Team member 07" /> */}
+                <h4 className="text-xl font-medium mb-1">Sirca Alexandru</h4>
+                <div className="text-gray-500 mb-1">Hardware Technician</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@quentinrenvoye</a> */}
               </div>
             </div>
 
             {/* 8th member */}
             <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="700" data-aos-anchor="[data-aos-id-team]">
               <div className="flex flex-col items-center">
-                <Image className="rounded-full mb-4" src={TeamImage08} width={120} height={120} alt="Team member 08" />
-                <h4 className="text-xl font-medium mb-1">Alyssa Chuzeville</h4>
-                <div className="text-gray-500 mb-1">Community Lead</div>
-                <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@hello_chuzeville</a>
+                {/* <Image className="rounded-full mb-4" src={TeamImage08} width={120} height={120} alt="Team member 08" /> */}
+                <h4 className="text-xl font-medium mb-1">Erick Kindlein</h4>
+                <div className="text-gray-500 mb-1">Sales Manager</div>
+                {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@hello_chuzeville</a> */}
               </div>
             </div>
 
