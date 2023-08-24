8f1758fcde675433cf69441479404ce39ca6bb54
Frank <andreass.frankk@gmail.com>
2023-08-24 22:20:05 +0300

    fixed lorem

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -7,7 +7,7 @@ export default function Timeline() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Bringing the world's ideas to life</h2>
-            <p className="text-xl text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className="text-xl text-gray-400">Pentru noi, la BlockArch, si cele mai proaste idei pot deveni povesti de viata.</p>
           </div>
 
           {/* Items */}
