dfe93a86b78ab91092a2ffc3574734227c51d8a3
Frank <andreass.frankk@gmail.com>
2023-12-04 19:12:07 +0200

    made responsive

--- app/(default)/ajutor-4x4/gallery.js
+++ app/(default)/ajutor-4x4/gallery.js
@@ -1,13 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const NextIcon = () => {
   return (
     <svg
-      width="80"
-      height="94"
+      width="40" // Adjusted width for mobile
+      height="47" // Adjusted height for mobile
       viewBox="0 0 80 94"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -16,7 +16,7 @@ const NextIcon = () => {
         d="M58.3736 55.777L21.5895 73.6151V65.1392L48.7344 52.7855L48.4574 53.2564V52.0653L48.7344 52.5639L21.5895 40.1825V31.7344L58.3736 49.5724V55.777Z"
         fill="white"
       />
-      <circle cx="40" cy="51" r="38.5" stroke="white" stroke-width="3" />
+      <circle cx="40" cy="51" r="38.5" stroke="white" strokeWidth="3" />
     </svg>
   );
 };
@@ -24,8 +24,8 @@ const NextIcon = () => {
 const PrevIcon = () => {
   return (
     <svg
-      width="80"
-      height="94"
+      width="40" // Adjusted width for mobile
+      height="47" // Adjusted height for mobile
       viewBox="0 0 80 94"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -34,13 +34,34 @@ const PrevIcon = () => {
         d="M17.5895 55.777V49.5724L54.3736 31.7344V40.1825L27.2287 52.5639L27.478 52.0653V53.2564L27.2287 52.7855L54.3736 65.1392V73.6151L17.5895 55.777Z"
         fill="white"
       />
-      <circle cx="40" cy="51" r="38.5" stroke="white" stroke-width="3" />
+      <circle cx="40" cy="51" r="38.5" stroke="white" strokeWidth="3" />
     </svg>
   );
 };
 
+// ... (PrevIcon and NextIcon components remain unchanged)
+
 const Gallery = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Check if the screen width is less than a certain threshold (e.g., 600px)
+      setIsMobile(window.innerWidth < 600);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Initial check on component mount
+    handleResize();
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -58,7 +79,7 @@ const Gallery = ({ images }) => {
         <button onClick={prevImage}>
           <PrevIcon />
         </button>
-        {Array.from({ length: 3 }).map((_, index) => {
+        {Array.from({ length: isMobile ? 1 : 3 }).map((_, index) => {
           const imageIndex = (currentIndex + index) % images.length;
           return (
             <Image
@@ -70,6 +91,7 @@ const Gallery = ({ images }) => {
                 height: "300px",
                 margin: "0 20px",
                 borderRadius: "4px",
+                display: isMobile && index > 0 ? "none" : "inline", // Hide additional images on mobile
               }}
             />
           );
