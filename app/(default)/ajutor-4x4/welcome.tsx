90248530eec014d36d1965df431a18f5ecaf557d
Frank <andreass.frankk@gmail.com>
2023-12-13 14:14:00 +0200

    added laurentiu

--- app/(default)/ajutor-4x4/welcome.tsx
+++ app/(default)/ajutor-4x4/welcome.tsx
@@ -87,9 +87,6 @@ const InstagramSVG = () => {
 };
 
 export default function SignUp() {
-  const THREE_DAYS_IN_MS = 24 * 24 * 60 * 60 * 1000 - 2 * (60 * 60 * 1000);
-  const NOW_IN_MS = new Date().getTime();
-
   const dateTimeAfterThreeDays = 1703781253000;
 
   const images = [image1, image2, image3, image4];
--- components/team.tsx
+++ components/team.tsx
@@ -1,20 +1,19 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import TeamImage01 from '@/public/images/team-member-01.jpg'
-import TeamImage02 from '@/public/images/team-member-02.jpg'
-import TeamImage03 from '@/public/images/team-member-03.jpg'
-import TeamImage04 from '@/public/images/team-member-04.jpg'
-import TeamImage05 from '@/public/images/team-member-05.jpg'
-import TeamImage06 from '@/public/images/team-member-06.jpg'
-import TeamImage07 from '@/public/images/team-member-07.jpg'
-import TeamImage08 from '@/public/images/team-member-08.jpg'
+import TeamImage01 from "@/public/images/team-member-01.jpg";
+import TeamImage02 from "@/public/images/team-member-02.jpg";
+import TeamImage03 from "@/public/images/team-member-03.jpg";
+import TeamImage04 from "@/public/images/team-member-04.jpg";
+import TeamImage05 from "@/public/images/team-member-05.jpg";
+import TeamImage06 from "@/public/images/team-member-06.jpg";
+import TeamImage07 from "@/public/images/team-member-07.jpg";
+import TeamImage08 from "@/public/images/team-member-08.jpg";
 
 export default function Team() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
-
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Echipa</h2>
@@ -22,10 +21,16 @@ export default function Team() {
           </div>
 
           {/* Team members */}
-          <div className="sm:flex sm:flex-wrap sm:justify-center -my-4 sm:-my-8 sm:-mx-3" data-aos-id-team>
-
+          <div
+            className="sm:flex sm:flex-wrap sm:justify-center -my-4 sm:-my-8 sm:-mx-3"
+            data-aos-id-team
+          >
             {/* 1st member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage01} width={120} height={120} alt="Team member 01" /> */}
                 <h4 className="text-xl font-medium mb-1">Tut Alexandru</h4>
@@ -35,7 +40,12 @@ export default function Team() {
             </div>
 
             {/* 2nd member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="100"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage02} width={120} height={120} alt="Team member 02" /> */}
                 <h4 className="text-xl font-medium mb-1">Frank Andreas</h4>
@@ -45,7 +55,12 @@ export default function Team() {
             </div>
 
             {/* 3rd member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage03} width={120} height={120} alt="Team member 03" /> */}
                 <h4 className="text-xl font-medium mb-1">Gabor Sebastian</h4>
@@ -55,7 +70,12 @@ export default function Team() {
             </div>
 
             {/* 4th member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="300"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage04} width={120} height={120} alt="Team member 04" /> */}
                 <h4 className="text-xl font-medium mb-1">Diana Deget</h4>
@@ -65,17 +85,27 @@ export default function Team() {
             </div>
 
             {/* 5th member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="400"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage05} width={120} height={120} alt="Team member 05" /> */}
-                <h4 className="text-xl font-medium mb-1">David Krisztian</h4>
-                <div className="text-gray-500 mb-1">Frontend Developer</div>
+                <h4 className="text-xl font-medium mb-1">Macovei Laurentiu</h4>
+                <div className="text-gray-500 mb-1">Python Developer</div>
                 {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@dominikprasad</a> */}
               </div>
             </div>
 
             {/* 6th member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage06} width={120} height={120} alt="Team member 06" /> */}
                 <h4 className="text-xl font-medium mb-1">Alex Moholea</h4>
@@ -85,7 +115,12 @@ export default function Team() {
             </div>
 
             {/* 7th member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="600" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="600"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage07} width={120} height={120} alt="Team member 07" /> */}
                 <h4 className="text-xl font-medium mb-1">Sirca Alexandru</h4>
@@ -95,7 +130,12 @@ export default function Team() {
             </div>
 
             {/* 8th member */}
-            <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3" data-aos="fade-up" data-aos-delay="700" data-aos-anchor="[data-aos-id-team]">
+            <div
+              className="sm:w-1/2 md:w-1/3 lg:w-1/4 py-4 sm:py-8 sm:px-3"
+              data-aos="fade-up"
+              data-aos-delay="700"
+              data-aos-anchor="[data-aos-id-team]"
+            >
               <div className="flex flex-col items-center">
                 {/* <Image className="rounded-full mb-4" src={TeamImage08} width={120} height={120} alt="Team member 08" /> */}
                 <h4 className="text-xl font-medium mb-1">Erick Kindlein</h4>
@@ -103,11 +143,9 @@ export default function Team() {
                 {/* <a className="block text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">@hello_chuzeville</a> */}
               </div>
             </div>
-
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
